2280566ea782d042b5888d240b07094b43f83d72
dkartaschew <chewy509@mailcity.com>
2012-10-08 10:16:47 +1000

    Fix JUnit tests in Eclipse.

--- src/com/anonymous/tests/activity/MainActivityTest.java
+++ src/com/anonymous/tests/activity/MainActivityTest.java
@@ -9,16 +9,15 @@ import static org.junit.Assert.*;
 
 @RunWith(SampleTestRunner.class)
 public class MainActivityTest {
-	
+
 	/**
 	 * Basic test to ensure the testing harness works.
+	 * 
 	 * @throws Exception
 	 */
 	@Test
-    public void shouldHaveHappySmiles() throws Exception {
-        String hello = new MainActivity().getString(R.string.app_name);
-        //assertThat(hello, equalTo("Solar Calculator for Android"));
-	assertTrue("Main Activity Name", hello.compareTo("Solar Calculator for Android") == 0);
-    }
-	
+	public void shouldHaveHappySmiles() throws Exception {
+		String hello = new MainActivity().getString(R.string.app_name);
+		assertTrue("Main Activity Name", hello.compareTo("Solar Calculator for Android") == 0);
+	}
 }
